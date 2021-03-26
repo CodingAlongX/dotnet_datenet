258b0807f4416ddccf93d44c84b12dc499cee931
S. M. JAHANGIR <smjxpro@gmail.com>
2021-03-26 20:31:10 +0600

    creating the register method in our auth controller
    Signed-off-by: S. M. JAHANGIR <smjxpro@gmail.com>

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,8 @@ namespace API
             services.AddDbContext<DataContext>(builder =>
                 builder.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "API", Version = "v1"}); });
+            services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
