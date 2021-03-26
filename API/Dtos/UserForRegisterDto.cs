5364f04b5b01a7da67cae616aa6b76d6c7afc020
S. M. JAHANGIR <smjxpro@gmail.com>
2021-03-26 20:46:00 +0600

    validation in the api
    Signed-off-by: S. M. JAHANGIR <smjxpro@gmail.com>

--- API/Dtos/UserForRegisterDto.cs
+++ API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class UserForRegisterDto
     {
-        public string Username { get; set; }
+        [Required] public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters")]
         public string Password { get; set; }
     }
 }
