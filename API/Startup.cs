f2b261ca6ee97949bdfa53fe1a7b2923a6359a1d
S. M. JAHANGIR <smjxpro@gmail.com>
2021-03-26 23:21:19 +0600

    creating the users controller
    Signed-off-by: S. M. JAHANGIR <smjxpro@gmail.com>

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,7 +35,11 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
             services.AddDbContext<DataContext>(builder =>
                 builder.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "API", Version = "v1"}); });
